0873d81e45e7c97ceb8d8c6a97839dec4b103760
Igor Gomes <igoorgoomes1996@gmail.com>
2022-06-30 23:07:30 -0300

    corrigindo erros

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const app = express();
 
 // Configuração da primeira rota
 //app.use("/", (req,res) => res.send("Olá mundo"));
-app.get("/home", function (req,res){
+app.get("/", function (req,res){
     res.sendFile(__dirname + "/html/index.html");
 });
 
-// rota para pagina projetos
-app.get("/projetos", function (req,res){
-    res.sendFile(__dirname + "/html/projetos.html");
-});
-
-// rota para pagina contatos
-app.get("/contatos", function (req,res){
-    res.sendFile(__dirname + "/html/contatos.html");
-});
+//// rota para pagina projetos
+//app.get("/projetos", function (req,res){
+//    res.sendFile(__dirname + "/html/projetos.html");
+//});
+//
+//// rota para pagina contatos
+//app.get("/contatos", function (req,res){
+//    res.sendFile(__dirname + "/html/contatos.html");
+//});
 // Informando ao node qual porta ele deve usar
 app.listen(process.env.PORT || 3000, console.log("Servidor Up"));
 
