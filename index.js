5a11d3e2a9657bd7bb7cd766fb8e279187f1d68e
Igor Gomes <igoorgoomes1996@gmail.com>
2022-06-30 22:36:18 -0300

    corrigindo ajuste na rota principal

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 
 // Configuração da primeira rota
 // app.use("/", express.static(__dirname + "/sua-pasta"))
-//app.use("/", (req,res) => res.send());
-app.use("/", express.static(__dirname + "/index.html"));
+app.use("/", (req,res) => res.sendFile("/index.html"));
+//app.use("/", express.static(__dirname + "/index.html"));
 
 // Informando ao node qual porta ele deve usar
 app.listen(process.env.PORT || 3000, console.log("Servidor Up"));
