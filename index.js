d047e6007abf20ffe6f5445c0fc8df05be902fbe
Igor Gomes <igoorgoomes1996@gmail.com>
2022-06-30 22:48:51 -0300

    corrigindo arquivos

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const express = require("express");
 const app = express();
 
 // Configuração da primeira rota
-// app.use("/", express.static(__dirname + "/sua-pasta"))
-app.use("/", (req,res) => res.sendFile("/index.html"));
-//app.use("/", express.static(__dirname + "/index.html"));
+//app.use("/", (req,res) => res.send("Olá mundo"));
+app.get("/", function (req,res){
+    res.sendFile(__dirname + "/html/index.html");
+});
 
 // Informando ao node qual porta ele deve usar
 app.listen(process.env.PORT || 3000, console.log("Servidor Up"));
